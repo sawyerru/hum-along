3bc89588909d3eeab62853e61ce31a08083949d8
Akash Godbole <akash.godbole16@gmail.com>
2020-09-19 00:07:08 -0400

    Added 4 buttons

---
+++ components/CardComponent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {View, StyleSheet, Button, Text} from "react-native";
+import {NotificationIcon} from "./VisualObjects";
+
+export default function Card(props){
+    return (
+        <View style={}>
+            <View style={styles.tileContainer}>
+                <Button title = "Song 1" ></Button>
+                <Button title = "Song 2" ></Button>
+                <Button title = "Song 3" ></Button>
+                <Button title = "Song 4" ></Button>
+            </View>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    tileContainer: {
+        marginHorizontal: 18,
+        marginVertical: 10,
+    },
+    notify: {
+        position: 'relative',
+        elevation: 3,
+        shadowOffset: {width: 1, height: 1},
+        shadowColor: '#333',
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        alignSelf: 'flex-end'
+    }
+})
