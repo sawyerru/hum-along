5efc6506700b968297464c31278a151922aa8fde
ErichHairston <Woohoo404!!>
2020-09-19 14:58:41 -0400

    ClockComponent count down

--- components/ClockComponent.js
+++ components/ClockComponent.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-export default function ClockCounter(props) {
-    return (
-        <View>
-            <Text>This is my Clock</Text>
-            <Text>{props.t}</Text>
-        </View>
-    )
+export default class ClockCounter extends Component{
+    constructor(props){
+        super(props);
+        this.state ={ seconds: props.t}
+    }
+    componentDidMount (){
+        this.interval = setInterval(
+            () => {
+            this.setState({
+                seconds: this.state.seconds - 1
+            })
+        }, 1000);
+    }
+    componentDidUpdate(){
+        if(this.state.seconds === 0){ 
+          clearInterval(this.interval);
+        }
+      }
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+    render () {
+        return (
+          <View >
+                <Text style ={{textAlignVertical: "center",textAlign: "center",
+                fontWeight: 'bold',
+                fontSize: 24,
+                marginTop: 0,
+                width: "100%", }}
+                >{this.state.seconds===0 ? 'Time is up! Pass to the next person': this.state.seconds}</Text>
+            </View>
+        )
+        
+    }
+    
 }
