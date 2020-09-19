f9e7d2dbe47cba5723f0c007aff1ffa8565a8a38
Akash Godbole <akash.godbole16@gmail.com>
2020-09-19 01:11:17 -0400

    Added temp songs

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import  Card from '../components/CardComponent'
 
 export default function GameScreen() {
     return (
         <View>
+            <Card></Card>
             <Text>This is the game screen</Text>
         </View>
     )
