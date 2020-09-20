093a484b608818be8cce8952338b50818b99f766
Akash Godbole <akash.godbole16@gmail.com>
2020-09-19 21:42:25 -0400

    Tried some random things

---
+++ database/mongo.py
@@ -0,0 +1,9 @@
+import csv
+
+fp = open("top50.csv", encoding='utf8')
+
+# reader = csv.reader(fp)
+
+# for line in reader:
+#     print(line)
+
---
+++ database/players.js
@@ -0,0 +1,6 @@
+export const players = [
+    {name: "Akash", key: '1'},
+    {name: "Erich", key: '2'},
+    {name: "Sawyer", key: '3'},
+    {name: "Scott", key: '4'},
+]
